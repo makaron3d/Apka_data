0d49bf1e38c7b906fa09e212962bb5d6689c7592
makaron3d <makaron3d@gmail.com>
2017-11-03 22:50:26 +0100

    0012

--- script.js
+++ script.js
@@ -57,6 +57,7 @@ function averages(...toAdd) {
 }
     console.log(averages(...arrayOfNumbers));
 
+
 switch (result) {
     case 1:
         console.log('Twoja liczba to cyfra 1');
@@ -89,6 +90,7 @@ switch (result) {
         console.log('Twoją liczbą narodzin jest mistrzowska liczba "11"');
         function finito() {
             return '<h2> mistrzowska liczba: ' + result + '</h2>';
+            personNumber.innerHTML += finito();
         }
         break; 
     case 22:
@@ -105,12 +107,151 @@ switch (result) {
         break;
     default:
         if ((result > 9) && (result != 11) && (result != 22) && (result != 33)) {
-            function finito() {
-                return '<h2>liczymy dalej: ' + result + '</h2>';
+            var nextResult = result;
+            console.log(nextResult);
+            var nextWynik = '' + nextResult;
+            console.log(nextWynik);
+            var arrayOfNextWynik = Array.from(nextWynik);
+            console.log(arrayOfNextWynik);
+            var arrayOfNextNumbers = arrayOfNextWynik.map(Number);
+            console.log(arrayOfNextNumbers);
+            let nextLevel = 0;
+            function sumNextLevel(...Add) {
+                for (let i = 0; i < Add.length; i++) {
+                    nextLevel += Add[i];
+                } 
+                return nextLevel;
+            }
+            console.log(sumNextLevel(...arrayOfNextNumbers));
+            function end () {
+                return '<h2> liczba: ' + nextLevel + '</h2>';
+            } 
+            
+            switch (nextLevel) {
+                case 1:
+                    console.log('Twoja liczba to cyfra 1');
+                    break; 
+                case 2:
+                    console.log('Twoja liczba to cyfra 2');
+                    break;
+                case 3:
+                    console.log('Twoja liczba to cyfra 3');
+                    break;
+                case 4:
+                    console.log('Twoja liczba to cyfra 4');
+                    break;
+                case 5:
+                    console.log('Twoja liczba to cyfra 5');
+                    break;
+                case 6:
+                    console.log('Twoja liczba to cyfra 6');
+                    break;   
+                case 7:
+                    console.log('Twoja liczba to cyfra 7');
+                    break;
+                case 8:
+                    console.log('Twoja liczba to cyfra 8');
+                    break;
+                case 9:
+                    console.log('Twoja liczba to cyfra 9');
+                    break; 
+                default:
+                    if (nextLevel > 9){
+                        console.log('3-ci poziom');//dalszy ciag, 3Ci poziom. 
+                        var nextLevelResult = nextLevel;
+                        console.log(nextLevelResult);
+                        var nextLevelWynik = '' + nextLevelResult;
+                        console.log(nextLevelWynik);
+                        var arrayOfNextLevelWynik = Array.from(nextLevelWynik);
+                        console.log(arrayOfNextLevelWynik);
+                        var arrayOfNextLevelNumbers = arrayOfNextLevelWynik.map(Number);
+                        console.log(arrayOfNextLevelNumbers);
+                        let lastLevel = 0;
+                        function sumlastLevel(...Adder) {
+                            for (let i = 0; i < Adder.length; i++) {
+                            lastLevel += Adder[i];
+                            } 
+                            return lastLevel;
+                        }
+                        console.log(sumlastLevel(...arrayOfNextLevelNumbers));
+                        function lastEnd () {
+                            return '<h2> liczba: ' + lastLevel + '</h2>';
+                        }                         
+                        switch (lastLevel) {
+                            case 1:
+                                console.log('Twoja liczba to cyfra 1');
+                                break; 
+                            case 2:
+                                console.log('Twoja liczba to cyfra 2');
+                                break;
+                            case 3:
+                                console.log('Twoja liczba to cyfra 3');
+                                break;
+                            case 4:
+                                console.log('Twoja liczba to cyfra 4');
+                                break;
+                            case 5:
+                                console.log('Twoja liczba to cyfra 5');
+                                break;
+                            case 6:
+                                console.log('Twoja liczba to cyfra 6');
+                                break;   
+                            case 7:
+                                console.log('Twoja liczba to cyfra 7');
+                                break;
+                            case 8:
+                                console.log('Twoja liczba to cyfra 8');
+                                break;
+                            case 9:
+                                console.log('Twoja liczba to cyfra 9');
+                                break; 
+                        }
+                    } 
+                }
         }
-    }
-} 
+}
+personNumber.innerHTML += lastEnd();
+//console.log(nextLevel);
 
-suma.onclick = personNumber.innerHTML += finito();
 
 
+//suma.onclick = 
+
+//personNumber.innerHTML += bla();
+
+
+//personNumber.innerHTML += finito();
+
+
+/*
+var nextResult = result;
+console.log(nextResult);
+
+var nextWynik = '' + nextResult;
+console.log(nextWynik);
+
+var arrayOfNextWynik = Array.from(nextWynik);
+
+console.log(arrayOfNextWynik);
+
+var arrayOfNextNumbers = arrayOfNextWynik.map(Number);
+console.log(arrayOfNextNumbers);
+
+
+
+let nextLevel = 0;
+
+function sumNextLevel(...Add) {
+    for (let i = 0; i < Add.length; i++) {
+        nextLevel += Add[i];
+    } 
+    return nextLevel;
+}
+
+console.log(sumNextLevel(...arrayOfNextNumbers));
+
+*/
+
+
+//console.log(averages(...arrayOfNextNumbers));
+//console.log(result);
